7005330b100ef3a85e174ce68d18c47f1a2d17f7
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-25 02:20:54 +0600

    Add custom memory allocation using malloc and free

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Custom memory (heap) allocation\n");
+int main() {
+    int *arr = (int *)malloc(10 * sizeof(int)); // Use malloc from the standard library
+    if (arr == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        arr[i] = (i + 1) * (i + 1);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+    
+    free(arr);
     return 0;
 }
